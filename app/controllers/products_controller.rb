78c7fa9bbb190f47214442b558a1eee5564bcc5f
miguel jimenez <denkatalabeno@gmail.com>
2020-03-27 10:12:30 +0800

    test in green!

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,27 @@
 class ProductsController < ApplicationController
 
   skip_before_action :authenticate_user!, only: :index
+  before_action :product_params, only: [:create]
 
   def index
     @products = Product.all
+  end
+
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save!
+    redirect_to root_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :tagline)
   end
 
 end
